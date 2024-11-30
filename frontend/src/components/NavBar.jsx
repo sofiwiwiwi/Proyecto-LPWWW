a30c0049d360a5cbb16f032a75a42dc342382de4
Florencia Ramírez <74277163+floraciendo@users.noreply.github.com>
2024-11-30 14:44:26 -0300

    diseño navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,15 +6,15 @@ const NavBar = ({ user, logout, isLoggedIn }) => {
     <nav class="navbar">
     <div class="navbar-container">
         <div class="navbar-brand">
-            <img src="cardiogram.png" alt="Logo" width="30" height="30"/>
+            <img src="cardiogram.png" alt="Logo" width="32" height="32"/>
             <span>Centro Médico Galenos</span>
         </div>
         {isLoggedIn && 
         <div className="d-flex align-items-center">
-          <span className="navbar-text me-3">
+          <span className="navbar-text">
             Bienvenido, {user?.name || "Desconocido"}
           </span>
-          <button onClick={logout}>
+          <button class="logout-button" onClick={logout}>
             Cerrar Sesión
           </button>
         </div>}
