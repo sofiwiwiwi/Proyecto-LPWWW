d04dbcf1efab746be0b068b2ebbdf96553cd51e0
Florencia Ramírez <74277163+floraciendo@users.noreply.github.com>
2024-11-30 15:01:35 -0300

    navbar responsivo ?

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,22 +4,23 @@ const NavBar = ({ user, logout, isLoggedIn }) => {
   return (
 
     <nav class="navbar">
-    <div class="navbar-container">
+      <div class="navbar-container">
         <div class="navbar-brand">
-            <img src="cardiogram.png" alt="Logo" width="32" height="32"/>
-            <span>Centro Médico Galenos</span>
+          <img src="cardiogram.png" alt="Logo" width="32" height="32" />
+          <span>Centro Médico Galenos</span>
         </div>
-        {isLoggedIn && 
-        <div className="d-flex align-items-center">
-          <span className="navbar-text">
-            Bienvenido, {user?.name || "Desconocido"}
-          </span>
-          <button class="logout-button" onClick={logout}>
-            Cerrar Sesión
-          </button>
-        </div>}
-    </div>
-</nav>
+        {isLoggedIn &&
+          <div className="d-flex align-items-center">
+            <span className="navbar-text">
+              Bienvenido, {user?.name || "Desconocido"}
+            </span>
+            <button class="logout-button" onClick={logout}>
+              <span className="logout-text">Cerrar Sesión</span>
+              <span className="logout-icon">✖</span>
+            </button>
+          </div>}
+      </div>
+    </nav>
   );
 };
 
